a6c2a54669792534c87ad56c6b3c0b3de728463a
Emory Merryman <emory.merryman@gmail.com>
2017-01-16 23:04:40 +0000

    fixed expirty script

--- src/expiry.sh
+++ src/expiry.sh
@@ -1,57 +1,51 @@
 #!/bin/sh
 
 cd ~/.password-store &&
-    git ls-tree --name-only -r master | while read FILE
+    for FILE in $(git ls-tree --name-only -r master | grep "[.]gpg\$")
     do
-	[ -z "$(git log --after $(date -d "${@}" +%Y-%m-%d) --follow ${FILE})" ] &&
+	
+	[ -z "$(git log --after $(date -d "${1}" +%Y-%m-%d) --follow ${FILE})" ] &&
+	    echo &&
 	    echo ${FILE%.*} has expired. &&
 	    PLAIN=$(pass show ${FILE%.*}) &&
 	    echo ${PLAIN} &&
-	    input_cmd(){
-		read -m "(I)nsert (E)dit (G)enerate (R)m (M)v (C)p (O)verlook (T)ouch" CMD &&
-		    case ${CMD} in
-			I)
-			    pass insert ${FILE%.*} &&
-				true
-			    ;;
-			E)
-			    pass edit ${FILE%.*} &&
-				true
-			    ;;
-			G)
-			    pass generate --no-symbols ${FILE%.*} ${#PLAIN} &&
-				true
-			    ;;
-			R)
-			    pass rm ${FILE%.*} &&
-				true
-			    ;;
-			M)
-			    read -m "NEW PATH" NEW_PATH &&
-				pass mv ${FILE%.*} ${NEW_PATH} &&
-				true
-			    ;;
-			C)
-			    read -m "NEW PATH" NEW_PATH &&
-				pass cp ${FILE%.*} ${NEW_PATH} &&
-				true
-			    ;;
-			O)
-			    true
-			    ;;
-			T)
-			    touch ~/password-store/${FILE} &&
-				pass git commit -am "TOUCHED ${FILE%.*} to unexpire." &&
-				true
-			    ;;
-			*)
-			    input_cmd &&
-				true
-			    ;;
-		    esac &&
+	    read -p "(I)nsert (E)dit (G)enerate (R)m (M)v (C)p (O)verlook (T)ouch  " CMD &&
+	    case ${CMD} in
+		I)
+		    pass insert ${FILE%.*} &&
+			true
+		    ;;
+		E)
+		    pass edit ${FILE%.*} &&
+			true
+		    ;;
+		G)
+		    pass generate --no-symbols ${FILE%.*} ${#PLAIN} &&
+			true
+		    ;;
+		R)
+		    pass rm ${FILE%.*} &&
+			true
+		    ;;
+		M)
+		    read -p "NEW PATH" NEW_PATH &&
+			pass mv ${FILE%.*} ${NEW_PATH} &&
+			true
+		    ;;
+		C)
+		    read -p "NEW PATH" NEW_PATH &&
+			pass cp ${FILE%.*} ${NEW_PATH} &&
+			true
+		    ;;
+		O)
 		    true
-	    } &&
-	    input_cmd &&
+		    ;;
+		T)
+		    touch ~/password-store/${FILE} &&
+			pass git commit -am "TOUCHED ${FILE%.*} to unexpire." &&
+			true
+		    ;;
+	    esac &&
 	    true
     done &&
     true
